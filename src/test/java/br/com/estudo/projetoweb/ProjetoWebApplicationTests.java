8fdcdda228de81ae20e298fde5d8acba4f52831b
Xcorpiion <vinicius.da.silva.cruz,sanji@gmail.com>
2020-05-10 00:44:36 -0300

    Iniciando projeto

---
+++ src/test/java/br/com/estudo/projetoweb/ProjetoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.estudo.projetoweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
